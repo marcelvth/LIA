d89c9981f101c3e37cc9cef0eb8935393ea00a6b
marcelvth <37689054+marcelvth@users.noreply.github.com>
2018-06-01 09:30:34 +0200

    2.0

---
+++ LIA2/app/src/main/java/com/bignerdranch/andriod/lia/VoorzieningenActivity.java
@@ -0,0 +1,14 @@
+package com.bignerdranch.andriod.lia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VoorzieningenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_voorzieningen);
+        setTitle("Voorzieningen");
+    }
+}
